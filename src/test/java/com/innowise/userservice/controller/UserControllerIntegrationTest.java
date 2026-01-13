4e8a08d945eef0e74def08f135b12613275b05ed
mxrpheus6 <pkazachenkoo@gmail.com>
2026-01-13 16:12:47 +0300

    Fix CI test fail

--- src/test/java/com/innowise/userservice/controller/UserControllerIntegrationTest.java
+++ src/test/java/com/innowise/userservice/controller/UserControllerIntegrationTest.java
@@ -26,8 +26,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.GenericContainer;
